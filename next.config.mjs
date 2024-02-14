13a8a9ff2b827b37533e7eb0ad1b94bf05f93b8c
rishikesh <rishikeshmoon@gmail.com>
2024-02-14 23:37:14 +0530

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["nnttssmmhwchbgowayzr.supabase.co"],
+  },
+};
 
 export default nextConfig;
